4d023c1937caa6ba18e0408735da874631e6a645
Micaela Ybarra <mica.yaz03@gmail.com>
2026-02-23 22:30:34 -0300

    WHY DOES IT NOT WORK IM LOSING MY MIND

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,20 +2,17 @@ import { View, TextInput, Button, Text } from "react-native";
 import { useState } from "react";
 import api from "../api/api";
 
-export default function LoginScreen({ navigation }) {
+export default function LoginScreen({}) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  //  fetch("http://192.168.43.100:3000/users/login", {
 
     const login = () => {
-        const res = fetch("http://192.168.43.100:3000/users/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: "test@test.com",
-            password: "123456"
-        })
+        fetch("http://192.168.43.100:3000/users/login", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
         })
         .then(r => r.json())
         .then(d => console.log("OK:", d))
@@ -25,12 +22,25 @@ export default function LoginScreen({ navigation }) {
   return (
     <View style={{ padding: 20 }}>
       <Text>Email</Text>
-      <TextInput onChangeText={setEmail} style={{ borderWidth: 1 }} />
+      <TextInput inputMode="email" autoCapitalize="none" onChangeText={setEmail} style={{ borderWidth: 1 }} />
 
       <Text>Password</Text>
-      <TextInput secureTextEntry onChangeText={setPassword} style={{ borderWidth: 1 }} />
+      <TextInput autoCapitalize="none" secureTextEntry onChangeText={setPassword} style={{ borderWidth: 1 }} />
 
       <Button title="Login" onPress={login} />
     </View>
   );
 }
+
+// { "code": 400, 
+//   "data": 
+//   [
+//     {"location": "body",
+//     "msg": "Email o contraseña incorrectos",
+//     "path": "email", 
+//     "type": "field",
+//     "value": "email"}
+//   ],  
+//   "message": "Error: parámetros incorrectos", 
+//   "success": false
+// }
